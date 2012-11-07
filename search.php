4127d595032f72e97e6bcf91df79387ec010bbf0
Nathan Germer <germer.n@gmail.com>
2012-11-07 16:39:40 -0600

    maybe now?

--- search.php
+++ search.php
@@ -2,14 +2,14 @@
 session_start();
 require "database.php";
 
-$stmt = $mysqli->prepare("SELECT page_title, page_namespace FROM page WHERE page_title LIKE '%?%' LIMIT 10");
+$stmt = $mysqli->prepare("SELECT page_title, page_namespace FROM page WHERE page_title LIKE ? LIMIT 10");
 
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit;
 }
 
-$query = $_GET['query'];
+$query = '%'.$_GET['query'].'%';
 
 
 $stmt->bind_param('s', $query);
