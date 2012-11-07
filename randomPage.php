1fb866bea79553fbeff8d0b6c2b6e7f94644e15f
Nathan Germer <germer.n@gmail.com>
2012-11-07 11:53:53 -0600

    random page selector.

---
+++ randomPage.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require "database.php";
+
+$rnd=mt_rand() / mt_getrandmax();ß
+
+$stmt = $mysqli->prepare("SELECT page_title FROM page WHERE page_namespace=0
+													AND page_is_redirect=0 AND page_random>$rnd LIMIT 1");
+
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+$stmt->execute();
+$stmt->bind_result($page_title);
+
+if($stmt->fetch()) {
+
+	$page_titles = array();
+	
+	array_push($page_titles, $page_title);
+
+	echo json_encode($page_title);
+
+}
+
+$stmt->close();
+
+exit;
+?>
