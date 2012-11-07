db0f22e002b9467beb07acda128a54bb1de40dad
Nathan Germer <germer.n@gmail.com>
2012-11-07 12:36:54 -0600

    now with 100% more works

--- getCategoryLinks.php
+++ getCategoryLinks.php
@@ -2,50 +2,35 @@
 session_start();
 require "database.php";
 
-$stmt = $mysqli->prepare("SELECT cat_id FROM category WHERE cat_title=?");
-
-if(!$stmt){
-	printf("Query Prep Failed: %s\n", $mysqli->error);
-	exit;
-}
-
 $category_title = $_GET['category'];
 
+$results = array();
+$results['cat'] = array();
+$results['page'] = array();
+
+$stmt = $mysqli->prepare("SELECT page.page_id, cl_type FROM categorylinks LEFT JOIN
+                          page ON page.page_id=cl_from WHERE cl_to=?");
+
+if(!$stmt){
+  printf("Query Prep Failed: %s\n", $mysqli->error);
+  exit;
+}
 
 $stmt->bind_param('s', $category_title);
 $stmt->execute();
-$stmt->bind_result($cat_id);
+$stmt->bind_result($title_to, $type);
 
-if($stmt->fetch()) {
-	$stmt->close();
-
-	$results = array();
-	$results['cat'] = array();
-	$results['page'] = array();
-
-	$stmt = $mysqli->prepare("SELECT cl_to, cl_type FROM categorylinks WHERE cl_from=?");
-
-	if(!$stmt){
-		printf("Query Prep Failed: %s\n", $mysqli->error);
-		exit;
-	}
-
-	$stmt->bind_param('s', $cat_id);
-	$stmt->execute();
-	$stmt->bind_result($title_to, $type);
-
-	while($stmt->fetch()) {
-		if($type=='subcat'){
-			array_push($results['cat'], $title_to);
-		} else if ($type=='page') {
-			array_push($results['page'], $title_to);
-		}
-	}
-
-	echo json_encode($results);
-
-	$stmt->close();
+while($stmt->fetch()) {
+  if($type=='subcat'){
+    array_push($results['cat'], $title_to);
+  } else if ($type=='page') {
+    array_push($results['page'], $title_to);
+  }
 }
+
+echo json_encode($results);
+
+$stmt->close();
 
 exit;
 ?>
