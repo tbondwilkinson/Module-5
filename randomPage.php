afa9ab300c3a34f24ec83878789caf3b95f42879
Nathan Germer <germer.n@gmail.com>
2012-11-07 12:00:54 -0600

    now with more random!

--- randomPage.php
+++ randomPage.php
@@ -5,7 +5,8 @@ require "database.php";
 $rnd=mt_rand() / mt_getrandmax();
 
 $stmt = $mysqli->prepare("SELECT page_title FROM page WHERE page_namespace=0
-													AND page_is_redirect=0 AND page_random>$rnd LIMIT 1");
+													AND page_is_redirect=0 AND page_random>$rnd ORDER BY
+													page_random LIMIT 1");
 
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
