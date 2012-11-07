db22bd6b9d257d3295c05d7efe6c5df8ab499e05
Nathan Germer <germer.n@gmail.com>
2012-11-07 12:54:02 -0600

    searchhhhhh

---
+++ search.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require "database.php";
+
+$stmt = $mysqli->prepare("SELECT page_title, page_namespace FROM page WHERE page_title LIKE '%?%' LIMIT 10");
+
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+$query = $_GET['query'];
+
+
+$stmt->bind_param('s', $query);
+$stmt->execute();
+$stmt->bind_result($title, $namespace);
+
+$result = array();
+
+while($stmt->fetch()) {
+	echo $title.' '.$namespace.'\n';
+}
+
+echo json_encode($array);
+
+$stmt->close();
+
+exit;
+?>
