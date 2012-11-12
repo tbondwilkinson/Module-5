fb4ba344c4516d740d5f1003277806b647ffcb94
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-11 20:40:56 -0600

    t

--- getPageLinks.php
+++ getPageLinks.php
@@ -2,7 +2,7 @@
 session_start();
 require "database.php";
 
-$stmt = $mysqli->prepare("SELECT page_id FROM page WHERE page_title=? AND page_namespace=0");
+$stmt = $mysqli->prepare("SELECT page_id FROM page WHERE page_title=? AND page_namespace=?");
 
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -12,7 +12,7 @@ if(!$stmt){
 $page_title = $_GET['post_title'];
 
 
-$stmt->bind_param('s', $page_title);
+$stmt->bind_param('sd', $page_title, 0);
 $stmt->execute();
 $stmt->bind_result($page_id);
 
@@ -21,14 +21,14 @@ if($stmt->fetch()) {
 
 	$page_links = array();
 
-	$stmt = $mysqli->prepare("SELECT pl_title FROM pagelinks WHERE pl_from=? AND page_namespace=0");
+	$stmt = $mysqli->prepare("SELECT pl_title FROM pagelinks WHERE pl_from=? AND page_namespace=?");
 
 	if(!$stmt){
 		printf("Query Prep Failed: %s\n", $mysqli->error);
 		exit;
 	}
 
-	$stmt->bind_param('s', $page_id);
+	$stmt->bind_param('sd', $page_id, 0);
 	$stmt->execute();
 	$stmt->bind_result($pl_title);
 	while($stmt->fetch()) {
