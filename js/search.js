305bc7018373a47f7e58e53e16f7aaa4278c8e3b
Nathan Germer <germer.n@gmail.com>
2012-11-11 21:31:39 -0600

    search

--- js/search.js
+++ js/search.js
@@ -1,6 +1,22 @@
 var searchmanager = {};
 
+
+
 //initialize on page buttons
 $().ready(function(){
+  //handle focus
+  $('#search').focus(function(event){
+    $('#searchpop').show('Blind');
+  });
+
+  //handle unfocus
+  $('#search').blur(function(event){
+    $('#searchpop').hide('Blind');
+  });
+
+  //handle search
+  $('#search').focus(function(event){
+    alert('searchbox has'+$('search').val());
+  });
 
 });
