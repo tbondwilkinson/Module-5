c8be948aee9ecfbd682f6569deda9016bd0f4cc4
Nathan Germer <germer.n@gmail.com>
2012-11-11 21:56:40 -0600

    ghhggh

--- js/search.js
+++ js/search.js
@@ -1,6 +1,22 @@
 var searchmanager = {};
 
+searchmanager.execsearch = function() {
+  var querystr = $('#search').val();
 
+  //let's goooooo
+  $.get("search.php", { query: querystr }, searchmanager.searchcallback);
+}
+
+searchmanager.searchcallback = function(data) {
+  var results = JSON.parse(data);
+
+  $('#searchpop').html('');
+
+  for(var i=0; i < results.length; i++) {
+    var string = results.title+((results.namespace==0)?"":" (CATEGORY)");
+    $('#searchpop').append(string+" ");
+  }
+}
 
 //initialize on page buttons
 $().ready(function(){
@@ -16,7 +32,7 @@ $().ready(function(){
 
   //handle search
   $('#search').keypress(function(event){
-    alert(event.currentTarget.value);
+    searchmanager.execsearch();
   });
 
 });
