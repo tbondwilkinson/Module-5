77ff14a9c30f29453942525ef3c64c75887d8cad
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-13 12:03:07 -0600

    t

--- wikiParse.php
+++ wikiParse.php
@@ -37,7 +37,7 @@ foreach ($items as $item) {
 }
 
 //get all p
-$items = $dom_xpath->query("//*[@id='mw-content-text']//p");
+$items = $dom_xpath->query("//*[@id='mw-content-text']/p");
 
 //json encode:
 if ($items->length > 0)
