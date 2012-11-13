12799b6c8d199248892426ab8439dfecfce56102
Nathan Germer <germer.n@gmail.com>
2012-11-13 00:02:35 -0600

    gg

---
+++ js/wiki_parse.php
@@ -0,0 +1,36 @@
+<?php
+$url="http://simple.wikipedia.org/wiki/"+$_GET['title'];
+
+//load in the page
+$str = file_get_contents(urlencode($url));
+$DOM = new DOMDocument;
+$DOM->loadHTML($str);
+
+$jsondata = array();
+$jsondata['title'] = "";
+$jsondata['img'] = "";
+$jsondata['summary'] = "";
+
+//get all h1
+$items = $DOM->getElementsByTagName('h1');
+
+//json encode:
+if ($items->length >0)
+	$jsondata['title']=$items->item(0)->nodeValue;
+
+//get all img
+$items = $DOM->getElementsByTagName('img');
+
+//json encode:
+if ($items->length >0)
+	$jsondata['img']=$items->item(0)->getAttribute('src');
+
+//get all p
+$items = $DOM->getElementsByTagName('p');
+
+//json encode:
+if ($items->length >0)
+	$jsondata['summary']=$items->item(0)->nodeValue;
+
+echo json_encode($jsondata);
+?>
