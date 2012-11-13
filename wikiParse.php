7debbb42c089e5507abfb0fcf3694425791e2e42
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-13 02:19:50 -0600

    t

--- wikiParse.php
+++ wikiParse.php
@@ -7,7 +7,7 @@ $url="http://simple.wikipedia.org/wiki/" . $_GET['title'];
 $html = file_get_html($url);
 
 // Find all images 
-$imgTW = $html->find('#infobox')->find("img");
+$imgTW = $html->find('#infobox', 0)->find("img", 0);
 
 echo $imgTW->src;
 
