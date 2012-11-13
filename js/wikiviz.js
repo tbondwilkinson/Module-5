c54f318740dc4ce31955003fdf20c8e57cb312a7
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-12 18:28:00 -0600

    t

--- js/wikiviz.js
+++ js/wikiviz.js
@@ -89,10 +89,15 @@ function loadPageLinks(data) {
         padX = 12;
         sizeOfFont = 12;
     }
-    else {
+    else if (pageLinks.length < 300) {
         padY = 5;
         padX = 5;
         sizeOfFont = 10;
+    }
+    else {
+        padY = 0;
+        padX = 0;
+        sizeOfFont = 8;
     }
 
 
