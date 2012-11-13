5047a74f32d94522c5e09f384d0ad5560235bff2
Nathan Germer <germer.n@gmail.com>
2012-11-13 10:35:19 -0600

    g

--- js/articlepopup.js
+++ js/articlepopup.js
@@ -45,19 +45,4 @@ articlepopmanager.showwikiarticle = function() {
 
 //initialize on page buttons
 $().ready(function(){
-  //handle focus
-  $('#search').focus(function(event){
-    $('#searchpop').show('Blind');
-  });
-
-  //handle unfocus
-  $('#search').blur(function(event){
-    $('#searchpop').hide('Blind');
-  });
-
-  //handle search
-  $('#search').keypress(function(event){
-    searchmanager.execsearch();
-  });
-
 });
