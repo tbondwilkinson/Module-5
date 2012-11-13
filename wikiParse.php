034b22c86f17c6553c762c82c4c21739e90866c8
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-13 10:48:26 -0600

    t

--- wikiParse.php
+++ wikiParse.php
@@ -21,9 +21,8 @@ if ($items->length >0)
 	$jsondata['title']=$items->item(0)->nodeValue;
 
 //get all img
-echo $DOM->saveXML();
 
-$items = $dom_xpath->query("//html/body/div[@id='mw-content-text']");//[@class='image']/img");
+$items = $dom_xpath->query("//*[@id='mw-content-text']");//[@class='image']/img");
 //json encode:
 print($items->length);
 if ($items->length > 0)
