699861c9a2d9eacb2ad63d2bbd19ee09de0ba51c
tbondwilkinson <tbondwilkinson@gmail.com>
2012-11-12 23:34:11 -0600

    t

--- js/wikiviz.js
+++ js/wikiviz.js
@@ -258,7 +258,8 @@ window.onload = function() {
         },
         dataType: "jsonp",
         success: function(jsonObject) {
-            alert(jsonObject.images[3].title);
+            var json = JSON.parse(jsonObject);
+            alert(json);
         }
     });
 };
